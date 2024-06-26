68209eddc18b51855e93cea8284deb4ced3a10aa
Felicia Alamorean <>
2024-06-26 13:44:39 +0300

    Added wish list tableView.

---
+++ LUFEL/LUFEL/Features/WishList/CellView/WishListTableViewCell.swift
@@ -0,0 +1,62 @@
+//
+//  WishListTableViewCell.swift
+//  LUFEL
+//
+//  Created by Felicia Alamorean on 26.06.2024.
+//
+
+import UIKit
+
+class WishListTableViewCell: UITableViewCell {
+
+    struct Identifier {
+        let imageUrl: URL?
+        let title: String
+        let price: String
+        let description: String?
+    }
+
+    // MARK: - Views
+    
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var addToCartButton: UIButton!
+
+    // MARK: - Lifecycle
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+
+    }
+
+    // MARK: - Actions
+
+    @IBAction func deleteProduct(_ sender: Any) {
+    }
+    
+    @IBAction func addProductToCart(_ sender: Any) {
+    }
+
+    // MARK: - Public methods
+
+    func configure(with identifier: Identifier) {
+        if let url = identifier.imageUrl {
+            productImageView.load(url: url)
+        }
+        nameLabel.text = identifier.title
+        priceLabel.text = identifier.price
+        descriptionLabel.text = identifier.description
+    }
+
+    // MARK: - Private methods
+
+}
--- LUFEL/LUFEL/Features/WishList/WishListViewController.swift
+++ LUFEL/LUFEL/Features/WishList/WishListViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class WishListViewController: UIViewController {
 
+    // MARK: - Views
+
+    @IBOutlet weak var tableView: UITableView!
+    
     // MARK: - Lifecycle
 
     override func loadView() {
@@ -18,12 +22,38 @@ class WishListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupTableView()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         (parent as? MainTabViewController)?.update(color: .black)
     }
+
+    // MARK: - Private methods
+
+    private func setupTableView() {
+        tableView.backgroundColor = UIColor.black
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+        tableView.register(WishListTableViewCell.self)
+        tableView.contentInset = .init(top: 0, left: 0, bottom: 0, right: 0)
+    }
+
+}
+extension WishListViewController: UITableViewDelegate {
+
+}
+
+extension WishListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
 }
